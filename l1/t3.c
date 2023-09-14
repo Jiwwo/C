c1319d5eec0cfd2cf6c47f2b4efe30c886827f1f
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2023-09-14 12:03:26 +0500

    /

--- l1/t3.c
+++ l1/t3.c
@@ -2,10 +2,8 @@
 
 int main(int argc, char *argv[]) {
     printf("Количество аргументов: %d\n", argc - 1);
-    
     for (int i = 1; i < argc; i++) {
         printf("Аргумент %d: %s\n", i, argv[i]);
     }
-    
     return 0;
 }
