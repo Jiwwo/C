6136834e36def4a85609247aebeac831bb46627c
Jiwwo <100707534+Jiwwo@users.noreply.github.com>
2023-09-21 15:54:50 +0500

    /

--- l1/t4.c
+++ l1/t4.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-int main(int argc, char *argv[]) { 
+int main(int *argc, char *argv[]) {
     char *filename = argv[1];
-    FILE *file = fopen(filename, "r");
-    char ch;
-    while ((ch = fgetc(file)) != EOF) {
-        putchar(ch);
-    }
-    fclose(file);
-    return 0;
+    FILE *fptr = fopen(filename, "r");
+    char myString[100];
+    fgets(myString, 100, fptr);
+    printf("%s", myString);
+    fclose(fptr); 
 }
